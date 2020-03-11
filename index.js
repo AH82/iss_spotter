8d90c6772b503f9c314ed73e27209b55ce49333e
Hatem <hatem.deux@gmail.com>
2020-03-11 14:26:07 -0400

    ISS Spotter IV

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
-// index.js
-// note : my home IP is 65.94.183.104 to use and it locates to montreal
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+// // index.js
+// // note : my home IP is 65.94.183.104 to use and it locates to montreal
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("IP fetching: It didn't work!" , error);
-    return;
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("IP fetching: It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('IP fetching: It worked! Returned IP:' , ip);
+// });
+
+
+// ////////////////////////////////////////////////////////////
+// const ip = "65.94.183.104";
+// // const ip = "65.9400.183.104";
+
+// /* fetchCoordsByIP(ip, (error, data) => {
+//   if (error) {
+//     console.log('coords fetching didnt work...\n', error);
+//     return;
+//   }
+
+//   console.log('coords fetching worked!\n coordinates are : ', data);
+// }); */
+// //////////////////////////////////
+//   // { latitude: '45.51150', longitude: '-73.56830' }
+
+//   const coords = { latitude: '45.51150', longitude: '-73.56830' };
+//   // const coords = { latitude: 'gg', longitude: '-73.56830' };
+// fetchISSFlyOverTimes(coords, (error, data) => {
+//   if (error) {
+//     console.log('ISSFlyOver fetching didnt work...\n', error);
+//     return;
+//   }
+
+//   console.log('ISSFlyOver fetching worked!\n', data);
+// });
+
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
+};
 
-  console.log('IP fetching: It worked! Returned IP:' , ip);
-});
-
-
-////////////////////////////////////////////////////////////
-const ip = "65.94.183.104";
-// const ip = "65.9400.183.104";
-
-/* fetchCoordsByIP(ip, (error, data) => {
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log('coords fetching didnt work...\n', error);
-    return;
+    return console.log("It didn't work!", error);
   }
-
-  console.log('coords fetching worked!\n coordinates are : ', data);
-}); */
-//////////////////////////////////
-  // { latitude: '45.51150', longitude: '-73.56830' }
-
-  const coords = { latitude: '45.51150', longitude: '-73.56830' };
-  // const coords = { latitude: 'gg', longitude: '-73.56830' };
-fetchISSFlyOverTimes(coords, (error, data) => {
-  if (error) {
-    console.log('ISSFlyOver fetching didnt work...\n', error);
-    return;
-  }
-
-  console.log('ISSFlyOver fetching worked!\n', data);
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
